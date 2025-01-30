4ac1dc8137bd5017051da9a7ab0101721054dcde
orlandoroca <rocahuapaya@hotmail.com>
2025-01-30 09:11:10 -0500

    first commit

---
+++ src/main/java/com/flabum/ludocolorbackend/LudoColorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flabum.ludocolorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudoColorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LudoColorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flabum/ludocolorbackend/LudoColorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flabum.ludocolorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudoColorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
