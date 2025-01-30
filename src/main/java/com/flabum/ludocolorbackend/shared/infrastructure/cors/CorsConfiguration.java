113e372c9e2a9fd362685895fa4d672d0a9d3ad1
orlandoroca <rocahuapaya@hotmail.com>
2025-01-30 18:22:47 -0500

    test

--- src/main/java/com/flabum/ludocolorbackend/shared/infrastructure/cors/CorsConfiguration.java
+++ src/main/java/com/flabum/ludocolorbackend/shared/infrastructure/cors/CorsConfiguration.java
@@ -13,7 +13,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173",
+                        .allowedOriginPatterns("http://localhost:5173",
                                 "https://ludocolor-backend-production.up.railway.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
